d58e99387bc059dce3df20a595e060c484f42279
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-21 16:08:45 +0500

    Registering models.

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -11,6 +11,11 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering= ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -21,6 +26,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
