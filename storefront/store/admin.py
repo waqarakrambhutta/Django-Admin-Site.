85d56a730c5f20a8ea6de55740012bbaf6913765
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-24 23:27:43 +0500

    trying to add computed columns.

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,19 +1,29 @@
 from django.contrib import admin
 from . import models
 
-@admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price','inventory_status']
-    list_editable=['unit_price']
-    list_per_page=10
+# @admin.register(models.Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ['title','unit_price','inventory_status']
+#     list_editable=['unit_price']
+#     list_per_page=10
     
-    @admin.display(ordering='-inventory')
-    def inventory_status(self, product):
-        if product.inventory < 10:
-            return 'low'
-        return 'OK'
+#     @admin.display(ordering='inventory')
+#     def inventory_status(self, product):
+#         if product.inventory < 10:
+#             return 'low'
+#         return 'OK'
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['placed_at','payment_status','id_inc']
+    list_per_page = 15
+    
+    def id_inc(self,order):
+        new_id = order.customer_id
+        return new_id + 1
 
 admin.site.register(models.Collection)
+# admin.site.register(models.Order,OrderAdmin)
 # admin.site.register(models.Product,ProductAdmin)
 
 
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -71,6 +71,12 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    def __str__(self) -> str:
+        return self.placed_at
+    
+    class Meta:
+        ordering = ['customer']
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
