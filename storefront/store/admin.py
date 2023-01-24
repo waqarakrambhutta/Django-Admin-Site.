f4d2f7da80b5faa0d04d96e7e033d5e6028efccd
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-24 18:19:05 +0500

    customising the list page.

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from . import models
 
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','unit_price']
+    list_editable=['unit_price']
+    list_per_page=10
+
+@admin.register(models.Customer) 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name','last_name','membership']
+    list_editable = ['membership']
+    ordering = ['first_name','last_name']
+    list_per_page = 10
+
+
 admin.site.register(models.Collection)
-admin.site.register(models.Product)
+# admin.site.register(models.Product,ProductAdmin)
+# admin.site.register(models.Customer,CustomerAdmin)
+
