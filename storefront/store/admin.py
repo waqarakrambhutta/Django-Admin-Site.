d2ba0102f2d6eb63b7f17d2b93d1783feb09e793
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-26 14:00:52 +0500

    overriding the base queries

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,30 +1,23 @@
 from django.contrib import admin
+from django.http import HttpRequest
+from django.db.models import Count
 from . import models
 
-# @admin.register(models.Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['title','unit_price','inventory_status','collection_title']
-#     list_editable=['unit_price']
-#     list_per_page=10
-#     list_select_related = ['collection']
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display =['title','product_count']
+
+    @admin.display(ordering='product_count')
+    def product_count(self,collection):
+        return collection.product_count
     
-#     def collection_title(self,product):
-#         return product.collection.title
-
-#     @admin.display(ordering='inventory')
-#     def inventory_status(self, product):
-#         if product.inventory < 10:
-#             return 'low'
-#         return 'OK'
+    def get_queryset(self,request):
+        return super().get_queryset(request).annotate(
+            product_count = Count('product')
+                                  
+        )
+        
 
 
-# admin.site.register(models.Order,OrderAdmin)
-# admin.site.register(models.Product,ProductAdmin)
-
-@admin.register(models.Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display =['id','placed_at','customer']
-    list_per_page = 15
-    list_select_related = ['customer']
-
-
+# admin.site.register(models.Collection,CollectionAdmin)
