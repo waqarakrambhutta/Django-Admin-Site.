4942636bdc550b0f41b7a76ecf152a8547c21640
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-26 12:15:42 +0500

    The Admin Site

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -3,29 +3,28 @@ from . import models
 
 # @admin.register(models.Product)
 # class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['title','unit_price','inventory_status']
+#     list_display = ['title','unit_price','inventory_status','collection_title']
 #     list_editable=['unit_price']
 #     list_per_page=10
+#     list_select_related = ['collection']
     
+#     def collection_title(self,product):
+#         return product.collection.title
+
 #     @admin.display(ordering='inventory')
 #     def inventory_status(self, product):
 #         if product.inventory < 10:
 #             return 'low'
 #         return 'OK'
 
-@admin.register(models.Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ['placed_at','payment_status','id_inc']
-    list_per_page = 15
-    
-    def id_inc(self,order):
-        new_id = order.customer_id
-        return new_id + 1
-
-admin.site.register(models.Collection)
-
 
 # admin.site.register(models.Order,OrderAdmin)
 # admin.site.register(models.Product,ProductAdmin)
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display =['id','placed_at','customer']
+    list_per_page = 15
+    list_select_related = ['customer']
 
 
--- storefront/store/models.py
+++ storefront/store/models.py
@@ -53,7 +53,10 @@ class Customer(models.Model):
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self) -> str:
-        return self.first_name
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Order(models.Model):
