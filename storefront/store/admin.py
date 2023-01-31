a237ce99d6a0225813775e0401720a53bf9a0bfb
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-01-31 15:13:17 +0500

    selecting related objects.

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
 from . import models
 
-@admin.register(models.Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','order']
-    list_select_related = ['order']
-
-    
+admin.site.register(models.CartItem)
