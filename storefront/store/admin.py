f791dbc3d305439e0f257f1b239a10c093d5f848
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-01 11:57:12 +0500

    adding link to the list page

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
+from django.db.models import Count
 from . import models
+from django.urls import reverse
+from django.utils.html import format_html
 
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','unit_price','last_update','collection_title']
-    list_select_related = ['collection']
-    def collection_title(self,link):
-        return link.collection.title
 
-admin.site.register(models.Product,ProductAdmin)
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display =  ['email','phone','order_count']
+    list_per_page = 10
+
+	
+    @admin.display(ordering='order_count')
+    def order_count(self,cart):
+        url = reverse('admin:store_customer_changelist')
+        return format_html('<a href="http://www.google.com">{}</a>',cart.order_count)
+    
+
+    def get_queryset(self,request):
+        return super().get_queryset(request).annotate(
+            order_count = Count('phone')
+                                  
+        )
