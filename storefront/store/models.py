b2400bab563c28f647a286c15bb9b26db6dcda74
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-02 15:18:32 +0500

    adding data validation

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
@@ -20,8 +21,13 @@ class Collection(models.Model):
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(null=True, blank= True)
+    unit_price = models.DecimalField(
+        max_digits=6, 
+        decimal_places=2,
+        validators=[MinValueValidator(1)
+        # validators=[MinValueValidator(1,message='beyond the scope')
+        ])
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
