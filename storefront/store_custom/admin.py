26e754ae41d996d47f171bde360f5ab2e0b739bb
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-05 12:53:32 +0500

    Revission starts here

--- storefront/store_custom/admin.py
+++ storefront/store_custom/admin.py
@@ -1,17 +1,3 @@
-from django.contrib.contenttypes.admin import GenericTabularInline
-from store.admin import ProductAdmin
-from tags.models import TaggedItem
 from django.contrib import admin
 
-from store.models import Product
-from .models import TaggedItem
-
-class TagInline(GenericTabularInline):
-    model = TaggedItem
-    autocomplete_fields = ['tag']
-
-class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline]
-admin.site.unregister(Product)
-admin.site.register(Product, CustomProductAdmin)
-
+# Register your models here.
