23cd67761b8a36031b48d27454172161a460c9b3
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-02-06 19:21:11 +0500

    revission

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,6 +3,8 @@ from store.models import Product
 
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    print(query_set)
-    return render(request, 'hello.html', {'name': 'Mosh','results':list(query_set)})
+    query_set = Product.objects.filter(collection__title__icontains='b')
+    
+    return render(request, 'hello.html', {'name': 'Mosh','results':query_set})
+    # return render(request, 'hello.html', {'name': 'Mosh','results':list(query_set)})
+
